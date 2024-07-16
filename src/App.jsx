620e7b65d7e9d135f8e50b0e54f2de6610eb4823
akhil-haridas <akhil.k@dignizant.com>
2024-07-16 15:06:36 +0530

    moved apryse to the public folder

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,18 @@ const App = () => {
   useEffect(() => {
     WebViewer(
       {
-        path: '/webviewer/lib',
-        licenseKey: 'demo:1721108165987:7f9a5c7b03000000007463b7c7a77a620e9c805eedc37b89008b5843ac', // sign up to get a key at https://dev.apryse.com
+        path: '../public',
+        licenseKey: 'demo:1721108165987:7f9a5c7b03000000007463b7c7a77a620e9c805eedc37b89008b5843ac',
         initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf',
       },
       viewer.current,
     ).then((instance) => {
       const { docViewer } = instance;
-      // you can now call WebViewer APIs here...
     });
   }, []);
 
   return (
     <div className="MyComponent">
-      <div className="header">React sample</div>
       <div className="webviewer" ref={viewer} style={{ height: "100vh" }}></div>
     </div>
   );
