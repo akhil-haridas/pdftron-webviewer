12ea3ed892b0735d60ee85f6ccef62261035f085
akhil-haridas <akhil.k@dignizant.com>
2024-07-16 14:57:58 +0530

    added ref div for the viewer

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,28 @@
 import React from 'react'
 
 const App = () => {
+  const viewer = useRef(null);
+
+  useEffect(() => {
+    WebViewer(
+      {
+        path: '/webviewer/lib',
+        licenseKey: 'demo:1721108165987:7f9a5c7b03000000007463b7c7a77a620e9c805eedc37b89008b5843ac', // sign up to get a key at https://dev.apryse.com
+        initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf',
+      },
+      viewer.current,
+    ).then((instance) => {
+      const { docViewer } = instance;
+      // you can now call WebViewer APIs here...
+    });
+  }, []);
+
   return (
-    <div>App</div>
-  )
+    <div className="MyComponent">
+      <div className="header">React sample</div>
+      <div className="webviewer" ref={viewer} style={{ height: "100vh" }}></div>
+    </div>
+  );
 }
 
 export default App
